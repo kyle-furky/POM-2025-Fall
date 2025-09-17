dd47785fec0ee4efefa60f0b381004a9f647c654
kyle-furky <kylefurky@gmail.com>
2025-09-17 00:56:04 -0400

    sep17 POM continues

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,4 +1,25 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    private WebDriver driver;
+
+    private By searchBox= By.id("searchbox");
+    private By searchButton=By.id("searchBtn");
+
+    public HomePage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public void enterSearchTest(String text){
+        driver.findElement(searchBox).sendKeys(text);
+    }
+
+    public void clickSearchButton(){
+        driver.findElement(searchButton).click();
+    }
+
+
 }
